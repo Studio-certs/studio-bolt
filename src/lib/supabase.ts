d8b08d9a708f51445e26c69f80470cfbe2d265f2
ChoudharyNavit22 <navitchoudhary22@gmail.com>
2025-01-24 13:48:05 +1100

    updated supabse creds

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL||'https://ltwdzsxpbythemeogbtm.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY||'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0d2R6c3hwYnl0aGVtZW9nYnRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY3MzgyMjEsImV4cCI6MjA1MjMxNDIyMX0.7yKDzBV7PFIwnsgcahDhQEkWI9KbXUTgfeelwu_Tu_w';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL||'https://alnfcdynlmiefgpulkjt.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY||'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsbmZjZHlubG1pZWZncHVsa2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NDY3MDMsImV4cCI6MjA1MzEyMjcwM30.ygpmbXWHEuZk0JvSQPrgX296HJn_jhGNodf175iG9D4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
