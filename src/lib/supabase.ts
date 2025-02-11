a89fb0896fc77247717e005a16a227082b97c488
ChoudharyNavit22 <navitchoudhary22@gmail.com>
2025-02-11 12:40:08 +1100

    db changes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL||'https://ydvvokjdlqpgpasrnwtd.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY||'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkdnZva2pkbHFwZ3Bhc3Jud3RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3NDQ1MDYsImV4cCI6MjA1NDMyMDUwNn0._UqHW22Xo6PaOXNaS5aN3MdS091Qjz25f5mTgWbqjq0';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL||'https://alnfcdynlmiefgpulkjt.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY||'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsbmZjZHlubG1pZWZncHVsa2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1NDY3MDMsImV4cCI6MjA1MzEyMjcwM30.ygpmbXWHEuZk0JvSQPrgX296HJn_jhGNodf175iG9D4';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
