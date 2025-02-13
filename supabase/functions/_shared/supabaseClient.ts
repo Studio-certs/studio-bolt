e9cb69d022686b08fffac52af25bb1ff3d899f83
ChoudharyNavit22 <navitchoudhary22@gmail.com>
2025-02-13 17:24:06 +1100

    default keys

--- supabase/functions/_shared/supabaseClient.ts
+++ supabase/functions/_shared/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const supabaseUrl = Deno.env.get('VITE_SUPABASE_URL') || '';
-const supabaseAnonKey = Deno.env.get('VITE_SUPABASE_ANON_KEY') || '';
+const supabaseUrl = Deno.env.get('VITE_SUPABASE_URL') || 'https://ydvvokjdlqpgpasrnwtd.supabase.co';
+const supabaseAnonKey = Deno.env.get('VITE_SUPABASE_ANON_KEY') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkdnZva2pkbHFwZ3Bhc3Jud3RkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg3NDQ1MDYsImV4cCI6MjA1NDMyMDUwNn0._UqHW22Xo6PaOXNaS5aN3MdS091Qjz25f5mTgWbqjq0';
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
